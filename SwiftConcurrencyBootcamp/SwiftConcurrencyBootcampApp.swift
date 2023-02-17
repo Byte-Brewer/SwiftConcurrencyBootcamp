011aea2244936d26bb407bd7cfc45703b0e26d19
NazarPrysiazhnyi <2008Nazar@gmail.com>
2023-02-17 18:50:36 +0200

    Initial Commit

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Nazar Prysiazhnyi on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
